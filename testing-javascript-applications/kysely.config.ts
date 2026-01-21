9155684968d94d2d9175732f21ff7a182ec0b3fc
muhammadali <muhammadalihassanofficial@gmail.com>
2026-01-21 11:04:10 +0500

    fix(testing-js-app): kysely migrations

--- testing-javascript-applications/kysely.config.ts
+++ testing-javascript-applications/kysely.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "kysely-ctl";
 import { CamelCasePlugin } from "kysely";
-import { join } from "path";
+import { dirname, join, resolve } from "path";
+import database from "better-sqlite3";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   dialect: "better-sqlite3",
+  dialectConfig: {
+    database: database(resolve(__dirname, "./src/db/sqlite.db")),
+  },
   plugins: [new CamelCasePlugin()],
   migrations: {
     migrationFolder: join("src/db", "migrations"),
--- testing-javascript-applications/src/db/migrations/1764825723804_create_cart.ts
+++ testing-javascript-applications/src/db/migrations/1764825723804_create_cart.ts
@@ -1,32 +1,26 @@
 import { sql, type Kysely } from "kysely";
 
-// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("cart")
-    .addColumn("id", "uuid", (c) =>
-      c.primaryKey().defaultTo(sql`gen_random_uuid()`)
-    )
+    .addColumn("id", "integer", (c) => c.primaryKey().autoIncrement())
     .addColumn("username", "varchar(150)", (c) => c.notNull())
-    .addUniqueConstraint("uniqueCartUsername", ["username"])
-    .addColumn("created_at", "timestamptz", (c) =>
-      c.notNull().defaultTo(sql`now()`)
+    .addColumn("created_at", "timestamp", (c) =>
+      c.notNull().defaultTo(sql`CURRENT_TIMESTAMP`),
     )
+    .addUniqueConstraint("uniqueCartUsername", ["username"])
     .execute();
 
   await db.schema
     .createTable("cart_item")
-    .addColumn("id", "uuid", (c) =>
-      c.primaryKey().defaultTo(sql`gen_random_uuid()`)
-    )
+    .addColumn("id", "integer", (c) => c.primaryKey().autoIncrement())
     .addColumn("name", "varchar(255)", (c) => c.notNull())
-    .addColumn("created_at", "timestamptz", (c) =>
-      c.notNull().defaultTo(sql`now()`)
+    .addColumn("created_at", "timestamp", (c) =>
+      c.notNull().defaultTo(sql`CURRENT_TIMESTAMP`),
     )
     .execute();
 }
 
-// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function down(db: Kysely<any>): Promise<void> {
   await db.schema.dropTable("cart_item").execute();
   await db.schema.dropTable("cart").execute();
